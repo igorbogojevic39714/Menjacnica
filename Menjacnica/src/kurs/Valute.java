6b2ea91dc52671f66a60f56e7885f4661fea5e64
igorbogojevic39714 <bogojevic.igor@gmail.com>
2016-03-28 01:27:12 +0200

    Generisane equals, hashCode i to String(izmenjena) metode.

--- Menjacnica/src/kurs/Valute.java
+++ Menjacnica/src/kurs/Valute.java
@@ -1,7 +1,7 @@
 package kurs;
 
 import java.util.GregorianCalendar;
-import java.util.LinkedList;
+
 
 public class Valute {
 	private String naziv;
@@ -45,6 +45,59 @@ public class Valute {
 	}
 	public void setKupovni(double kupovni) {
 		this.kupovni = kupovni;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valute other = (Valute) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valute [Naziv valute: " + naziv + ", skracenica: " + skracenica + ", tacan datum kursa: " + datum + ", Prodajni kurs: " + prodajni
+				+ ", Srednji kurs: " + srednji + ", Kupovni kurs:" + kupovni + "]";
 	}
 	
 }
