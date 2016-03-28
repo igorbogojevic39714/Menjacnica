f68c80c53be5c4ef6837ec1986f07cfaff561500
igorbogojevic39714 <bogojevic.igor@gmail.com>
2016-03-28 02:27:54 +0200

    U klasi KursImplementacija, sve metode su implementirane.

--- Menjacnica/src/KursImplementacija.java
+++ Menjacnica/src/KursImplementacija.java
@@ -8,17 +8,37 @@ public class KursImplementacija implements KursInterfejs {
 	private LinkedList<Valute> kursevi = new LinkedList<Valute>();
 	@Override
 	public void dodavanjeKursa(Valute v) {
-		
+		if (v != null){
+			kursevi.add(v);
+		}
+		else{
+			System.out.println("Greska pri unosu objekta");
+		}
 	}
 
 	@Override
 	public void brisanjeKursa(Valute v) {
-		
+		if (kursevi.isEmpty()){
+			return;
+		}
+		else {
+			if(kursevi.contains(v)){
+				kursevi.remove(v);
+			}
+			else{
+				System.out.println("Objekat koji je unesen ne postoji.");
+			}
+		}
 	}
 
 	@Override
 	public Valute pronadji(Valute v) {
-		
+		for (int i=0;i<kursevi.size();i++){
+			if (kursevi.get(i).getDatum().equals(v.getDatum()) && 
+					kursevi.get(i).getNaziv().equals(v.getNaziv())){
+				return v;
+			}
+		}
 		return null;
 	}
 
