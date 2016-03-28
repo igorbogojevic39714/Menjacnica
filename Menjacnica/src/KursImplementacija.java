c85488165d1a8799c814bf4bda55cc3da9f6c177
igorbogojevic39714 <bogojevic.igor@gmail.com>
2016-03-28 04:11:22 +0200

    (PROBA) Radim commit, jer progam nece da merge-uje dve grane.

--- Menjacnica/src/KursImplementacija.java
+++ Menjacnica/src/KursImplementacija.java
@@ -8,6 +8,17 @@ public class KursImplementacija implements KursInterfejs {
 	private LinkedList<Valute> kursevi = new LinkedList<Valute>();
 	@Override
 	public void dodavanjeKursa(Valute v) {
+		if (kursevi.contains(v)){
+			System.out.println("Vec postoji takav objekat");
+			return;
+		}
+		else{
+			kursevi.addLast(v);
+		}
+		if(kursevi.isEmpty()){
+			System.out.println("Lista je prazna");
+			kursevi.addLast(v);
+		}
 		
 	}
 
